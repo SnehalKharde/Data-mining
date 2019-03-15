b525b7d94474d572cfa8e739ecef2b7e52175de6
snehal.kharde <47136231+SnehalKharde@users.noreply.github.com>
2019-03-15 17:10:31 +0530

    Add files via upload
    Text emotion recognition

---
+++ Speech_Emotion_Recognition.R
@@ -0,0 +1,47 @@
+library(stringr)
+library(kernlab)
+data1 <- read.csv("/Users/Ajay/Desktop/t2.csv")
+
+train<-data1[1:1000,]
+test<-data1[3001:3009,]
+
+classifier <- ksvm(sentiment~., data = train, kernel = "rbfdot")
+
+prediction<-predict(classifier,test)
+
+length(prediction)
+length(test)
+View(prediction)
+
+table(prediction, test$sentiment)
+match = prediction == test$sentiment
+table(match)
+prop.table(table(match))
+
+mean(match)*100
+match<-table(prediction,test$sentiment)
+
+mean(match)*100
+View(match)
+prediction[1:10]
+test$sentiment[1:10]
+
+#match<- predict == test$sentiment
+
+View(match)
+
+ncol(train_data1)
+View(train_data1)
+column<-train_data1[,2]
+
+test <- str_replace_all(column,"[:punct:]","")
+test
+train_data1[,2]<-test 
+View(train_data1)
+dim(train_data1)
+training <- train_data1[1:7500,]
+classifier <- ksvm(sentiment~., data = training, kernel = "vanilladot")
+test_data1<-train_data1[7501:9000,]
+View(test_data1)
+
+Prediction <- predict(classifier,test_data1)
